22f5aecc769d13b4259a10ade1250c85266ae44a
Fox Scarlett <foxontheone@gmail.com>
2026-07-03 15:42:37 -0700

    layout, start header

---
+++ src/components/layouts/Header.tsx
@@ -0,0 +1,13 @@
+import styles from '../../styles/Header.module.css'
+
+function Header() {
+  return (
+    <div className={styles.site_header}>
+      <div className='container'>
+        <div>PilledUp</div>
+        <div>site nav</div>
+      </div>
+    </div>
+  )
+}
+export default Header
