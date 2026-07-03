14a947f01f2957da1537ed47bb67171dcc6d2797
Fox Scarlett <foxontheone@gmail.com>
2026-07-03 14:32:31 -0700

    initial commit after wiping frontpage

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <section id='center'>
+        <div>
+          <h1>PilledUp</h1>
+          <p>
+            enter a new world of security, relaxation, and confidence - you'll
+            love it
+          </p>
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default App
