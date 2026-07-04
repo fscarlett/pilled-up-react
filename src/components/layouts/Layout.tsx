2f35bfc4108872afffbed3f3c3afb3fddaa0d50e
Fox Scarlett <foxontheone@gmail.com>
2026-07-04 15:05:14 -0700

    1350 container

--- src/components/layouts/Layout.tsx
+++ src/components/layouts/Layout.tsx
@@ -4,7 +4,9 @@ function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div>
       <Header />
-      {children}
+      <main className='mx-auto w-full max-w-337.5 px-4'>
+        {children}
+      </main>
     </div>
   )
 }
